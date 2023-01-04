afa1a415f3d7703dcaffdf602b6d33bbe593091b
Chris <Christopherly45@gmail.com>
2023-01-04 17:24:07 +1100

    Init
    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, SafeAreaView } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+
+  const [randomDog, setDog] = useState({image: 'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_3673.jpg', breed: 'terrier irish'});
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      let imageUrl = data.message;
+      let breed = data.message.split('/')[4];
+      breed = breed.split('-').join(' ');
+      breed = breed.charAt(0).toUpperCase() + breed.slice(1);
+      
+      setDog({image: imageUrl, breed: breed});
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.textContainer, styles.shadow]}>
+        <Text style={{fontSize: 20}}>Hello, my breed is</Text>
+        <Text style={{fontWeight: 'bold', fontSize: 25}}>{randomDog.breed}</Text>
+      </View>
+      
+      <View style={[styles.imageContainer, styles.shadow]}>
+        <Image style={styles.image} source={{uri:randomDog.image}}/>
+      </View>
+
+      <View style={[styles.buttonContainer, styles.shadow]}>
+        <Button title='Random Dog 🐾' color="#000" onPress={clickHandler}/>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffaf1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  buttonContainer: {
+    width: 350,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    flex: 1,
+    position: 'absolute',
+    bottom: 70,
+  }, 
+
+  textContainer: {
+    marginTop: 20,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: 350,
+    marginBottom: 30,
+    alignItems: 'center',
+  },
+
+  imageContainer: {
+      width: 350,
+      height: 350,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 10,
+      marginBottom: 200,
+  },
+
+  image: {
+    width: 330,
+    height: 330,
+    borderRadius: 10,
+  },
+
+  shadow: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  }
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+## Introduction
+
+Welcome to the "Random Dogs" app! This app is built using [React Native](https://reactnative.dev/) and displays photos of random dogs.
+I built this as i am relearning React Native.  
+
+## Features
+
+-   The app uses the [Dog API](https://dog.ceo/dog-api/) to retrieve pictures of random dogs and displays them on the screen.
+-   The user can refresh the page to see a new dog picture.
+
+![App Screenshot](/App_Image.jpg)
